d88fd8cb7bcf1d5b2204cbfe74d993089c2cfd25
sapuru <sapuru@demeter>
2016-09-20 15:50:51 -0300

    comenzando

---
+++ src/obrasocial/ObraSocial.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+En una Obra Social, dada la edad de una persona, 
+de le asignará una determinada categoría; que será 
+“Recién Nacido” (menos de 1 año), 
+“Infante” (hasta 5 años), 
+“Niño” (de 6 a 12 años), 
+“Pre-adolescente” (más de 12 años, hasta 18); 
+“Adolescente” (más de 18 años, hasta 25), 
+“Adulto joven” (más de 25, hasta 35 años); 
+“Adulto” (más de 35, hasta 50 años) 
+o “Adulto mayor” (más de 50 años). 
+Dada la edad de una persona, informar su categoría.
+ */
+package obrasocial;
+
+/**
+ *
+ * @author sapuru
+ */
+public class ObraSocial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        int edad;
+        String cadenaEdad;
+        
+    }
+    
+}
